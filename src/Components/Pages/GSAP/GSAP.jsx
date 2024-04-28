898a52a6ad392609c3354bb1e657c33d95943a4f
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-28 10:47:56 +0600

    first commit

---
+++ src/Components/Pages/GSAP/GSAP.jsx
@@ -0,0 +1,12 @@
+import style from './style.css';
+const GSAP = () => {
+    return (
+        <div>
+
+            <div id='box'></div>
+
+        </div>
+    );
+};
+
+export default GSAP;
---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import GSAP from "../GSAP/GSAP";
+
+const Home = () => {
+    return (
+        <div>
+            <GSAP></GSAP>
+        </div>
+    );
+};
+
+export default Home;
