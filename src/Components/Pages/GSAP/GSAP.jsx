c97d249b965d26bfe3c0ebe6157e89727143b086
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-04-30 11:58:36 +0600

    add text animation

--- src/Components/Pages/GSAP/GSAP.jsx
+++ src/Components/Pages/GSAP/GSAP.jsx
@@ -1,9 +1,31 @@
 import style from './style.css';
+import gsap from "gsap";
+
 const GSAP = () => {
+
+
+    gsap.to('h1', {
+        fontSize: '100px',
+        color: 'blue',
+        duration: 5
+    })
+
+    gsap.from('h1', {
+        fontSize: '50px',
+        color: 'red',
+        delay: 5
+    })
+
+
     return (
         <div>
 
-            <div id='box'></div>
+
+            <div className='box' id='box'>
+                <h1>Hello Im piyal</h1>
+                <h1>Im Web Developer</h1>
+                <h1>Photograpper</h1>
+            </div>
 
         </div>
     );
