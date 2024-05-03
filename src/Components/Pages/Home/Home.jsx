7f2aaa5440f5111b3f93c62016029421ef16f700
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-03 15:25:42 +0600

    write code with gsap

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,12 +4,20 @@ import { useGSAP } from "@gsap/react";
 const Home = () => {
 
 
-    
+    useGSAP(() => {
+        gsap.from('#box', {
+            y: -50,
+            opacity: 0,
+            duration: 2
+        })
+    })
+
+
 
     return (
         <div>
 
-            <div id="box" className="w-[200px] h-[200px] bg-yellow-400 rounded-full">
+            <div id="box" className="w-[200px] h-[200px] bg-yellow-400 rounded-full mt-20">
 
             </div>
 
