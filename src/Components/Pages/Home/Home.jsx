bc13c85f7716bc2348c6200d0d73e19c8b3ecc40
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-05 15:06:16 +0600

    add scope

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -4,31 +4,33 @@ import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 const Home = () => {
 
-    const gsapRef = useRef();
-
-
 
     useGSAP(() => {
-        gsap.from(gsapRef.current, { opacity: 0, duration: 1, y: -100, ease: "power2.out" })
-    })
+        gsap.from('.box', {
+            y: 30,
+            opacity: 0,
+            duration: 1,
+            rotate: 720,
+
+        })
+    }, { scope: ".contan1" })
+
+
+
+
 
 
     return (
-        <div>
+        <div className="container mx-auto flex gap-10 justify-center items-center h-screen">
 
-            <div id="nav" className="flex justify-between items-center container m-auto mt-4">
+            <div className="contan1 border border-black p-20">
+                <div className="circle w-[200px] h-[200px] bg-yellow-300 rounded-full"></div>
+                <div className="box w-[200px] h-[200px] bg-red-400"></div>
+            </div>
 
-                <div ref={gsapRef} id="nav-logo" className="text-4xl font-extrabold">Logo</div>
-
-                <div id="nav-items" className="text-3xl font-bold text-green-600">
-                    <ul className="flex gap-6">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Contact</li>
-                        <li>Help</li>
-                        <li>Blog</li>
-                    </ul>
-                </div>
+            <div className="contan2">
+                <div className="circle w-[200px] h-[200px] bg-yellow-300 rounded-full"></div>
+                <div className="box w-[200px] h-[200px] bg-red-400"></div>
             </div>
 
         </div>
